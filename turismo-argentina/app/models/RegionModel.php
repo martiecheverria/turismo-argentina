225693731610093b8aeaeba9118b74d461bb097e
martiecheverria <martiecheverria2005@gmail.com>
2025-10-21 20:35:40 -0300

    Update RegionModel.php

--- turismo-argentina/app/models/RegionModel.php
+++ turismo-argentina/app/models/RegionModel.php
@@ -4,16 +4,11 @@ class RegionModel {
 
     public function __construct() {
         require_once 'config.php';
-        try {
-            $this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-        } catch (PDOException $e) {
-            if ($e->getCode() == 1049) { // Código de error SQL para "Unknown database"
-                header("Location: " . BASE_URL . "install.php");
-                die();
-            } else {
-                die("Error de conexión a la base de datos: " . $e->getMessage());
-            }
-        }
+        $this->db = new PDO(
+            "mysql:host=".MYSQL_HOST .
+            ";dbname=".MYSQL_DB.";charset=utf8", 
+            MYSQL_USER, MYSQL_PASS);
+            $this->deploy();
     }
 
     public function getAllRegiones() {
@@ -45,6 +40,7 @@ class RegionModel {
         $query->execute([$id_region]);
         return $query->fetch(PDO::FETCH_OBJ);
     }
+
 
 
 }
