829110c4ee62973fc2b106c41df61ead12964007
martiecheverria <martiecheverria2005@gmail.com>
2025-10-21 20:36:36 -0300

    Update UserModel.php

--- turismo-argentina/app/models/UserModel.php
+++ turismo-argentina/app/models/UserModel.php
@@ -5,16 +5,11 @@ class UserModel {
 
     public function __construct() {
         require_once 'config.php';
-        try {
-            $this->db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8', DB_USER, DB_PASS);
-        } catch (PDOException $e) {
-            if ($e->getCode() == 1049) { // Código de error SQL para "Unknown database"
-                header("Location: " . BASE_URL . "install.php");
-                die();
-            } else {
-                die("Error de conexión a la base de datos: " . $e->getMessage());
-            }
-        }
+        $this->db = new PDO(
+        "mysql:host=".MYSQL_HOST .
+        ";dbname=".MYSQL_DB.";charset=utf8", 
+        MYSQL_USER, MYSQL_PASS);
+
     }
 
 
@@ -31,4 +26,5 @@ class UserModel {
         $query = $this->db->prepare('INSERT INTO usuarios (email, password) VALUES (?, ?)');
         $query->execute([$email, $hash]);
     }
+
 }
